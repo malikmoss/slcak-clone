da0b943e5b44cb09337892df2e35a41159ae8000
Malik Moss <malikmoss33@gmail.com>
2021-08-05 17:34:52 -0500

    still resolving reference errors

---
+++ index.js
@@ -0,0 +1,20 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import {graphqlExpress, graphiqlExpress} from 'apollo-server-express';
+
+import typeDefs from './schema';
+import resolvers from './resolvers'
+
+export const schema = makeExecutableSchema({
+    typeDefs,
+    resolvers,
+})
+
+const app = express();
+const graphqlEndpoint = 'graphql';
+
+app.use('/graphql', bodyParser.json(), graphqlExpress({ schema }));
+
+app.use('/graphiql', graphiqlExpress({ endpointURL: '/graphql' }));
+
+app.listen(8080)

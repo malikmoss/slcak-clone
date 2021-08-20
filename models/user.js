59b258525dec238fca62cbea2704b951284d7259
Malik Moss <malikmoss33@gmail.com>
2021-08-19 22:28:37 -0500

    User associations

--- models/user.js
+++ models/user.js
@@ -2,21 +2,32 @@ export default (sequelize, DataTypes) => {
     const User = sequelize.define('user', {
         username: {
             type: DataTypes.STRING,
-            unique: true
+            unique: true,
         },
         email: {
             type: DataTypes.STRING,
             unique: true,
         },
-        password: DataTypes.STRING
+        password: DataTypes.STRING,
     });
 
     User.associate = (models) => {
         User.belongsToMany(models.Team, {
             through: 'member',
-            foreignKey: 'userId',
-        });
+            foreignKey: {
+              name: 'userId',
+              field: 'user_id',
+        },
+      });
     };
+    
+    User.belongsToMany(models.Channel, {
+        through: 'channel_member',
+        foreignKey: {
+            name: 'userId',
+            field: 'user_id'
+        }
+    })
 
     return User;
 }
